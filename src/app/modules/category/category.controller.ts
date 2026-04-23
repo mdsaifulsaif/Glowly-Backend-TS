a595e8fc7d06448ec87d190488f62d2e68b28cfc
Saiful Saif <mdsaifulislam02137@gmail.com>
2026-04-23 12:13:04 +0600

    implement getCategoryByProducts endpoint and update category routes and services

--- src/app/modules/category/category.controller.ts
+++ src/app/modules/category/category.controller.ts
@@ -77,8 +77,30 @@ const deleteCategory = catchAsync(async (req: Request, res: Response) => {
     data: null,
   });
 });
+
+
+const getCategoryByProducts = catchAsync(async (req: Request, res:Response)=>{
+  const {id} = req.params
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
+const result = await CategoryServices.getProductsByCategoryFromDB(id as string, page, limit);
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Products retrieved by category successfully!",
+    meta: result.meta,
+    data: result.data,
+  });
+
+})
+
+
 export const CategoryControllers = {
   createCategory,
   getCategories,
   deleteCategory,
+  getCategoryByProducts
 };
