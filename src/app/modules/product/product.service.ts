559904a3f2c4d535b1596d4d4c3d268b9520481d
Saiful Saif <mdsaifulislam02137@gmail.com>
2026-04-28 11:19:11 +0600

    added nodemailer env

--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -89,50 +89,6 @@ export const getNewProductsService = async (params: GetProductsParams) => {
   return products;
 };
 
-// const getAllProductsFromDB = async (query: Record<string, any>) => {
-//   const {
-//     searchTerm,
-//     category,
-//     page = 1,
-//     limit = 8,
-//     sort,
-//     ...filterData
-//   } = query;
-
-//   const filter: any = { ...filterData };
-
-//   if (searchTerm) {
-//     filter.$or = [
-//       { name: { $regex: searchTerm, $options: "i" } },
-//       { description: { $regex: searchTerm, $options: "i" } },
-//     ];
-//   }
-
-//   if (category && category !== "All Product") {
-//     filter.categoryID = category;
-//   }
-
-//   let sortStr = "-createdAt";
-//   if (sort) {
-//     sortStr = sort as string;
-//   }
-
-//   const skip = (Number(page) - 1) * Number(limit);
-
-//   const result = await Product.find(filter)
-//     .populate("categoryID")
-//     .sort(sortStr)
-//     .skip(skip)
-//     .limit(Number(limit));
-
-//   const total = await Product.countDocuments(filter);
-//   const totalPage = Math.ceil(total / Number(limit));
-
-//   return {
-//     meta: { page: Number(page), limit: Number(limit), total, totalPage },
-//     data: result,
-//   };
-// };
 
 const getAllProductsFromDB = async (query: Record<string, any>) => {
   const {
